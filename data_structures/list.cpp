922408411c3df3d6386052f3a8a37f0bf5bd4b62
jedrekikiNerd <80655226+jedrekikiNerd@users.noreply.github.com>
2024-03-16 01:47:38 +0100

    Added add_back()

--- data_structures/list.cpp
+++ data_structures/list.cpp
@@ -1,4 +1,5 @@
 #include "list.h"
+#include <iostream>
 
 template <typename Type>
 void LinkedListH<Type>::add_front(Type value)
@@ -8,16 +9,46 @@ void LinkedListH<Type>::add_front(Type value)
         head = new_node;
     else
     {
-        head->next = new_node;
+        new_node->next_element = head->next_element;
+        head = new_node;
     }
     size++;
 }
 
 template <typename Type>
-void add_back();
+void LinkedListH<Type>::add_back(Type value)
+{
+    Node<Type>* new_node = new Node<Type>(value);
+    if (head == nullptr)
+        head = new_node;
+    else
+    {
+        Node<Type>* last_node = &head;
+        while(last_node->next_element != nullptr)
+        {
+            last_node = last_node->next_element;
+        }
+        last_node->next_element = new_node;
+    }
+    size++;
+}
 
 template <typename Type>
-void add_at();
+void LinkedListH<Type>::add_at(Type value, int position)
+{
+    Node<Type>* new_node = new Node<Type>(value);
+    if (position >= size or position < 0)
+        std::cerr << "Position exceeds size!";
+    else
+    {
+        Node<Type>* currrent_node = head;
+        for(int i=0; i<position; i++)
+        {
+            // TODO: finish
+        }
+    }
+    size++;
+}
 
 template <typename Type>
 void remove_front();
@@ -48,3 +79,9 @@ void size();
 
 template <typename Type>
 void find();
+
+template <typename Type>
+void get_at();
+
+template <typename Type>
+void print();
